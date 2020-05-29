018b03d00994bcba6f31368f0a1c2332d026f1bb
Rodgers Chayuga <chayugarodgers@gmail.com>
2020-05-29 08:44:37 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/kingdom_ride/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kingdom_ride
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../requests/google_map_requests.dart';
+import '../utils/utilities.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_database/firebase_database.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+
+//import 'package:kingdom_ride/utils/utilities.dart';
+//import 'package:uuid/uuid.dart';
+// import 'package:google_maps_webservice/directions.dart';
+// import 'package:google_maps_webservice/distance.dart';
+// import 'package:google_maps_webservice/geocoding.dart';
+// import 'package:google_maps_webservice/geolocation.dart';
+// import 'package:google_maps_webservice/places.dart';
+// import 'package:google_maps_webservice/timezone.dart';
+
+//import 'package:http/http.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Map()
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
+
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  GoogleMapController mapController;
+  GoogleMapsServices _googleMapsServices = GoogleMapsServices();
+  static LatLng _initialPosition;
+  LatLng _lastPosition = _initialPosition;
+  final Set<Marker> _markers = {};
+  final Set<Polyline> _polylines = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          initialCameraPosition:
+              CameraPosition(target: _initialPosition, zoom: 20),
+          onMapCreated: onCreated,
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
+        ),
+
+        Positioned(
+          top: 50.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              //controller: appState.locationController,
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.black,
+                  ),
+                ),
+                hintText: "pick up",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+          top: 105.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              //controller: appState.destinationController,
+              //textInputAction: TextInputAction.go,
+              // onSubmitted: (value) {
+              //   appState.sendRequest(value);
+              // },
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.local_taxi,
+                    color: Colors.black,
+                  ),
+                ),
+                hintText: "destination?",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+
+        // Positioned(
+        //   top: 40,
+        //   right: 10,
+        //   child: FloatingActionButton(onPressed: _onAddMarkerPressed,
+        //   tooltip: "add marker",
+        //   backgroundColor: black,
+        //   child: Icon(Icons.add_location, color: white),
+        //   ),
+        // ),
+      ],
+    );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(title: "remember here", snippet: "good place"),
+          icon: BitmapDescriptor.defaultMarker));
+    });
+  }
+
+  // !DECODE POLY
+  List _decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList = new List();
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+    // repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result |= (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+      /* if value is negetive then bitwise not the value */
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    } while (index < len);
+
+/*adding to previous value as done in encoding */
+    for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+}
